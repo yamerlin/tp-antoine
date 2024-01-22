19ad56ce49db3be553d7d8479f70f5b570d98850
yamerlin <95913488+yamerlin@users.noreply.github.com>
2024-01-22 13:00:54 +0100

    Add files via upload

--- tp1.c
+++ tp1.c
@@ -3,8 +3,11 @@
 #include <stdbool.h>
 #include <string.h>
 
+//Le nodeId est un identifiant unique à chaque noeud
+//Il sera utilisé principalement pour choisir un noeud quand on veut le supprimer car c'est la seule manière de diffèrencier deux noeud ayant exactement la même date et la même heure
 int nodeId = 0;
 
+//Définition de la structure d'un noeud
 struct node{
     int year;
     int month;
@@ -16,9 +19,29 @@ struct node{
 
     struct node *next;
 };
+//Création du noeud de tête
 struct node *head = NULL;
 
+void printCalendar(){
+    struct node *testNode = head;
+    //Skipper le noeud de tete car il est bidon
+    testNode = testNode->next;
+    printf("\n");
+
+    while(testNode != NULL) {
+        printf("____\n");
+        printf("| Id de l'événement : %d\n", testNode->nodeId);
+        printf("| Date : %d/%d/%d à %dh \n", testNode->day, testNode->month, testNode->year, testNode->hour);
+        printf("| Description : %s\n", testNode->eventDescription);
+
+        //Passer au noeud suivant
+        testNode = testNode->next;
+    }
+    printf("\n");
+}
+
 void addEvent(int day, int month, int year, int hour, char eventDescription[100]){
+
     //Créer le nouveau noeud et lui allouer la mémoire
     struct node *newNode = NULL;
     newNode = malloc(sizeof(struct node));
@@ -30,16 +53,82 @@ void addEvent(int day, int month, int year, int hour, char eventDescription[100]
     //Boolean pour confirmer que la position du nouveau noeud a été trouvée
     bool isPosFound = false;
 
-    //Evaluation
+    //Evaluation de la position du nouveau noeud
     previousNode = testNode;
-    while(testNode != NULL && !isPosFound) {
-        
-        if (testNode->year > year){
+    do {
+        if (testNode->year == year){
+            //Si l'année est ègale on compare le mois
+
+            if(testNode->month == month){
+                //Si le mois est égale on compare le jour
+
+                if(testNode->day == day){
+                    //Si le jour est égale on compare l'heure
+
+                    if(testNode->hour >= hour){
+                        //Si l'heure est égale alors les deux événements sont égaux alors on le met avant par défaut
+
+                        //Retenir le noeud qui sera après
+                        nextNode = testNode;
+
+                        //Confirmer que la position a été trouvée
+                        isPosFound = true;
+                        printf("\nPlace trouvée\n");
+                    }
+                    else{
+                        //Retenir le noeud qui sera avant
+                        previousNode = testNode;
+
+                        //Avancer d'un noeud
+                        testNode = testNode->next;
+                    }
+                }
+
+                else if(testNode->day > day){
+                    //Si l'heure est plus grande on a trouvé la place
+
+                    //Retenir le noeud qui sera après
+                    nextNode = testNode;
+
+                    //Confirmer que la position a été trouvée
+                    isPosFound = true;
+                    printf("\nPlace trouvée\n");
+                }
+                else{
+                    //Retenir le noeud qui sera avant
+                    previousNode = testNode;
+
+                    //Avancer d'un noeud
+                    testNode = testNode->next;
+                }
+            }
+            else if(testNode->month > month){
+                //Si le mois est plus grand on a trouvé la place
+
+                //Retenir le noeud qui sera après
+                nextNode = testNode;
+
+                //Confirmer que la position a été trouvée
+                isPosFound = true;
+                printf("\nPlace trouvée\n");
+            }
+            else{
+                //Retenir le noeud qui sera avant
+                previousNode = testNode;
+
+                //Avancer d'un noeud
+                testNode = testNode->next;
+            }
+        }
+        else if(testNode->year > year){
+            //SI L'année est plus grande on a trouvé la place
+
             //Retenir le noeud qui sera après
             nextNode = testNode;
 
             //Confirmer que la position a été trouvée
             isPosFound = true;
+            printf("\nPlace trouvée\n");
         }
         else{
             //Retenir le noeud qui sera avant
@@ -48,7 +137,7 @@ void addEvent(int day, int month, int year, int hour, char eventDescription[100]
             //Avancer d'un noeud
             testNode = testNode->next;
         }
-    }
+    }while(testNode != NULL && !isPosFound);
 
     //Setter les valeurs du noeud d'avant le nouveau noeud
     previousNode->next = newNode;
@@ -114,12 +203,44 @@ void createEvent(){
 }
 
 void deleteEvent(){
+    int idOfEventToDelete;
+    bool isEventFound = false;
+    struct node *previousNode = head;
+    printf("\nVoici les événements disponibles : \n");
+    printCalendar();
+    printf("\nEntrez l'Id de l'événement que vous souhaitez supprimer :");
+    scanf("%d", &idOfEventToDelete);
+    printf("\n");
 
+    //Parcourir la liste chainée pour trouvé l'événement avec l'id correspondant
+    struct node *testNode = head;
+    previousNode = testNode;
+    //Skipper le noeud de tete car il est bidon
+    while(testNode != NULL && !isEventFound) {
+        if(testNode->nodeId == idOfEventToDelete){
+            //Pour supprimer un noeud, il suffit de relier le neoud d'avant avec le noeud suivant
+            previousNode->next = testNode->next;
+
+            isEventFound = true;
+        }
+
+        previousNode = testNode;
+        //Passer au noeud suivant
+        testNode = testNode->next;
+    }
+
+    if(!isEventFound){
+        printf("Le noeud n'a pas été trouvé, vérifiez si l'id est correct");
+    }
+    else{
+        printf("Noeud trouvé et supprimé !");
+    }
 }
 
 void saveCalendar(){
     char calendarName[100];
 
+    printf("\nVos calendriers sont enregistrés dans le dossier courant où le programme est executé\n");
     printf("Entrez le nom de votre calendrier : ");
     //Flusher les possible caracteres end of line restant dans le buffer d'input pour éviter le fgets() de se faire skipper si il choppe un eof ou eof
     char c; //Variable bidon
@@ -175,16 +296,31 @@ void loadCalendar(){
     char nodeIdStr[20] = "";
 
     char eventDescription[100];
+    char calendarName[100];
+
+    printf("\nVos calendriers doivent être dans le dossier courant où le programme est executé\n");
+    printf("Charger un calendrier le fusionnera avec le calendrier actuel\n");
+    printf("Entrez le nom de votre calendrier (/!\\ ne pas oublier le .txt /!\\) : ");
+    //Flusher les possible caracteres end of line restant dans le buffer d'input pour éviter le fgets() de se faire skipper si il choppe un eof ou eof
+    char c; //Variable bidon
+    while ((c = getchar()) != '\n' && c != EOF);
+    //Récuperer l'input utilisateur
+    fgets(calendarName, 100, stdin);
+
+    //Enlever le eol de calendarName
+    int len = strlen(calendarName);
+    calendarName[len-1]='\0';
 
     //Ouvrir le calendrier
     FILE *filePointer;
-    filePointer = fopen("test2.txt","r");
+    filePointer = fopen(calendarName,"r");
 
     //Vérifier que le calendrier existe
     if (filePointer == NULL) {
         printf("Le calendrier n'existe pas ou est corompu \n");
     }
     else{
+        //ch représente le caractère en cours de lecture dans le fichier
         ch = fgetc(filePointer);
 
         if(ch == '\n' || ch == EOF){
@@ -310,19 +446,17 @@ void loadCalendar(){
     }
 
     fclose(filePointer);
-}
 
-void printCalendar(){
+    //Ajuster le nodeId en fonction du plus élevé du calendrier chargé
+    //Pour éviter que deux noeud aient le même nodeId quand on en ajoutera par la suite
     struct node *testNode = head;
     //Skipper le noeud de tete car il est bidon
     testNode = testNode->next;
-    printf("\n");
-
+    //Défiler tout les noeuds
     while(testNode != NULL) {
-        printf("____\n");
-        printf("| Id de l'événement : %d\n", testNode->nodeId);
-        printf("| Date : %d/%d/%d à %dh \n", testNode->day, testNode->month, testNode->year, testNode->hour);
-        printf("| Description : %s\n", testNode->eventDescription);
+        if(testNode->nodeId > nodeId){
+            nodeId = testNode->nodeId + 1;
+        }
 
         //Passer au noeud suivant
         testNode = testNode->next;
@@ -335,28 +469,6 @@ int main() {
     //Init le noeud de tete
     head = malloc(sizeof(struct node));
     head->next = NULL;
-
-    // struct node *n1 = NULL;
-    // struct node *n2 = NULL;
-    // struct node *n3 = NULL;
-    // struct node *n4 = NULL;
-
-    // n1 = malloc(sizeof(struct node));
-    // n2 = malloc(sizeof(struct node));
-    // n3 = malloc(sizeof(struct node));
-    // n4 = malloc(sizeof(struct node));
-    
-
-    // n1->year = 1;
-    // n2->year = 2;
-    // n3->year = 4;
-    // n4->year = 5;
-
-    
-    // n1->next = n2;
-    // n2->next = n3;
-    // n3->next = n4;
-    // n4->next = NULL;
 
     int value;
 
